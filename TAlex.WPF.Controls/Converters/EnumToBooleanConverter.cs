b485f2cdd40ade50e0e30ad9413803e7618d3ae8
Alex Titarenko <alex.titarenko@talex-soft.com>
2013-07-13 01:02:45 +0300

    EnumToBooleanConverter has been added.

---
+++ TAlex.WPF.Controls/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+
+namespace TAlex.WPF.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(Boolean))]
+    public class EnumToBooleanConverter : ConverterBase<EnumToBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
